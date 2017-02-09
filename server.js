bde1a55d9ad33d11fcf36659b8bcdd46512b74a2
Prakash Tiwary <p.tiw191@gmail.com>
2017-02-09 17:07:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,8 +12,9 @@ app.get('/testing', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'testing.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
