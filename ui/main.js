4a5a53be2baccd7268615c7f49841cdc9ba5a772
Prakash Tiwary <p.tiw191@gmail.com>
2017-02-12 03:09:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,27 @@
 //console.log('Loaded!');
 var button = document.getElementById('counter');
-var counter=0;
+
 button.onclick= function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange = function(){
+    // process the server response
+    if (request.readyState === XMLHttpRequest.DONE) {
+        if(request.status==200){
+          var counter=request.responseText;
+          var span=document.getElementById('count');
+          span.innerHTML=counter.toString();
+            
+            
+        }
+    // everything is good, the response is received
+} else {
+    // still not ready
+}
+};
     
-    
-  //render the variable to correct span
-  counter=counter+1;
-  var span=document.getElementById('count');
-  span.innerHTML=counter.toString();
+  request.open('GET','http://tiwary191.imad.hasura-app.io/counter',true);
+  request.send(null);
+  
+ 
+  
 };
