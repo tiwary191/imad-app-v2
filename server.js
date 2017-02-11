703cfac769c9eddb41d95868c3aabc7b953ff11d
Prakash Tiwary <p.tiw191@gmail.com>
2017-02-12 04:15:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 var names= [];
-app.get('/submit-name/',function(req,res){
+app.get('/submit-name',function(req,res){
     
   var name=req.query.name;
   names.push(name);
